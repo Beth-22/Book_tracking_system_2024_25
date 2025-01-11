17568463bbc5ad987dabd49da3b3910917546e18
nettib <netsanettibebu098@gmail.com>
2025-01-12 01:10:26 +0300

    Delete backend/dist/Order directory

--- backend/dist/Order/dto/create-order.dto.d.ts
+++
@@ -1,9 +0,0 @@
-import { OrderState } from '../schema/order.schema';
-export declare class CreateOrderDto {
-    productId: string;
-    quantity: number;
-    customerName: string;
-    customerEmail: string;
-    state?: OrderState;
-    address: string;
-}
